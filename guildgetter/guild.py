f8f15f3c642fd9f3ace66b45cd279a7818a81897
Luke Rowe <RogShotz@users.noreply.github.com>
2020-06-25 13:53:20 -0700

    add exceptions

--- guildgetter/guild.py
+++ guildgetter/guild.py
@@ -1,8 +1,9 @@
+# Created by Dueffy 06/25/2020
+
 import requests
 
-API_KEY ="KEY" # your api key
-name = "dueffy" # username you want to get for
-
+API_KEY = "KEY"  # your api key
+name = "dueffy"  # username you want to get for
 
 url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
 res = requests.get(url)  # 'res' is the response to the request to the URL. We can get get data from our response
@@ -10,11 +11,16 @@ if res.status_code == 204:
     print("User: " + name + ", does not exist. Please check name variable")
 else:
     data = res.json()  # This will convert the JSON data into a Python object that we can parse
-    PUUID = (data["id"]) # player UUID
+    PUUID = (data["id"])  # player UUID
     url = f"https://api.hypixel.net/findGuild?key={API_KEY}&byUuid={PUUID}"
     res = requests.get(url)
-    data = res.json()
-    GUUID = (data["guild"])
-
-    print("Player UUID: " + PUUID)
-    print("Guild UUID: " + GUUID)
+    if res.status_code == 403:
+        print("BAD API KEY")
+    else:
+        data = res.json()
+        GUUID = (data["guild"])
+        if GUUID is None:
+            print("This player does not have a Guild")
+        else:
+            print("Player UUID: " + PUUID)
+            print("Guild UUID: " + GUUID)
