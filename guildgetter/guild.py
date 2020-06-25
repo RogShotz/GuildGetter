3afff6393ad08753ef441c9895e850f22d220e7f
Luke Rowe <RogShotz@users.noreply.github.com>
2020-06-25 13:37:05 -0700

    added a function to block invalid usernames

--- guildgetter/guild.py
+++ guildgetter/guild.py
@@ -1,16 +1,20 @@
 import requests
 
-API_KEY ="42bf9e6f-60fe-469d-bd75-c23b761eaf6a" # your api key
+API_KEY ="KEY" # your api key
 name = "dueffy" # username you want to get for
+
 
 url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
 res = requests.get(url)  # 'res' is the response to the request to the URL. We can get get data from our response
-data = res.json()  # This will convert the JSON data into a Python object that we can parse
-PUUID = (data["id"]) # player UUID
-url = f"https://api.hypixel.net/findGuild?key={API_KEY}&byUuid={PUUID}"
-res = requests.get(url)
-data = res.json()
-GUUID = (data["guild"])
+if res.status_code == 204:
+    print("User: " + name + ", does not exist. Please check name variable")
+else:
+    data = res.json()  # This will convert the JSON data into a Python object that we can parse
+    PUUID = (data["id"]) # player UUID
+    url = f"https://api.hypixel.net/findGuild?key={API_KEY}&byUuid={PUUID}"
+    res = requests.get(url)
+    data = res.json()
+    GUUID = (data["guild"])
 
-print("Player UUID: " + PUUID)
-print("Guild UUID: " + GUUID)
+    print("Player UUID: " + PUUID)
+    print("Guild UUID: " + GUUID)
